f55d66c90176e961b9311b1246d8256b2f0f654f
Tona <tona@mango-byte.com>
2022-09-24 13:38:00 +0700

    add articles

--- tests/GraphQL/ArticleTest.php
+++ tests/GraphQL/ArticleTest.php
@@ -21,4 +21,24 @@ class ArticleTest extends TestCase
         $this->assertEquals("Sint qui officiis veritatis.", $article['title']);
     }
 
+    /** @test */
+    public function testArticles(): void {
+        $response = $this->graphQL('{
+            articles {
+                id,
+                title,
+                content,
+                author {
+                 id,name,email
+                }
+            }
+        }');
+        $articles = $response['data']['articles'];
+        $this->assertEquals(1, $articles[0]['id']);
+        $this->assertEquals("Sint qui officiis veritatis.", $articles[0]['title']);
+
+        $this->assertEquals(1, $articles[0]['author']['id']);
+        $this->assertEquals("Tre Green DDS", $articles[0]['author']['name']);
+    }
+
 }
